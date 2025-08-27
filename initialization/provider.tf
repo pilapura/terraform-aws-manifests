be8efa3ea3f3236cdd0df72bf52d4e469b59caae
prashanth.ilapuram <prashanth0459@gmail.com>
2025-08-28 01:49:40 +0530

    terraform initialization

---
+++ initialization/provider.tf
@@ -0,0 +1,13 @@
+# Terraform sttings block (HCL)
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
